fd36eebbada1c9f1b9d9f9475add1c41922c8f49
Marlon Falcon <marlonfalcon@MacBook-Pro-de-Marlon.local>
2021-09-15 15:19:47 +0200

    terminando end-ponit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,52 @@
 const express = require('express');
 const app = express();
 const knoxTokenLibraryJs = require('knox-token-library-js');
+const URL_FILE = "keys/keys.json"
+var Constants = require('../keys/hash');
 
-app.get('/api/getsignedclientid', (req, res)=>{
-    res.send('Hello World')
+// Step 5
+app.get('/api/getsignedclientid/:clientId/:codehash', (req, res)=>{
+    if (req.params.codehash !== Constants.CODE_HASH) {
+        res.status(400).json({
+            msg: "Your Codehash is not valid"
+        });
+    } else {
+        try {
+            const clientId = req.params.clientId;
+            const signedClientId = knoxTokenLibraryJs.generateSignedClientIdentifierJWT(URL_FILE, clientId );
+            res.status(200).json({
+                signedClientId
+            });
+        } catch (error) {
+            es.status(400).json({
+                msg: 'error', error
+            });
+        }
+    }
+    
+    
+});
+
+// Step 7
+app.get('/api/signedaccesstoken/:apitoken/:codehash', (req, res)=>{
+    if (req.params.codehash !== Constants.CODE_HASH) {
+        res.status(400).json({
+            msg: "Your Codehash is not valid"
+        });
+    } 
+    else {
+        try {
+            const apitoken = req.params.apitoken;
+            const signedToken = knoxTokenLibraryJs.generateSignedAccessTokenJWT(URL_FILE, apitoken );
+            res.status(200).json({
+                signedToken
+            });
+        } catch (error) {
+            es.status(400).json({
+                msg: 'error', error
+            })
+    }
+    }
 });
 
 app.listen(3000, ()=>{
